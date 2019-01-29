c46dc0444e6c051509cf433f38fd87a67ef99b17
Jeff Kilpatrick <kilpatrick.jeff@gmail.com>
2019-01-29 07:12:27 -0400

    resync

--- Targets/7DaysToDie/Mods/Blooms_AnimalHusbandry/Scripts/EntityFarmingAnimal.cs
+++ Targets/7DaysToDie/Mods/Blooms_AnimalHusbandry/Scripts/EntityFarmingAnimal.cs
@@ -17,10 +17,21 @@ using System.IO;
 
 class EntityFarmingAnimal : EntityAnimalStag
 {
-    public String strFoodItem = "resourceYuccaFibers";
-    public String strProductItem = "water";
-    public String strHarvestItems = "drinkJarEmptyMilk,drinkJarEmptyMilk";
+    public String strFoodItem = "";
+    public String strProductItem = "";
+    public String strHarvestItems = "";
+    public String strHomeBlock = "";
+    private float HarvestDelay = 10f;
+
+    // how far the animal will wander from its Home position.
+    private int MaxDistanceFromHome = 15;
+    private float MaxDistanceToSeePlayer = 20f;
+
+    // Introduce some check delays to reduce the amount of calls per tick on them
     float CheckDelay = 5f;
+    float LongerDelay = 3600f;
+    float nextLongerCheck = 0;
+
     private float nextCheck = 0;
     private float ProductDelay = 2400f;
     private float productNextCheck = 0;
@@ -29,33 +40,40 @@ class EntityFarmingAnimal : EntityAnimalStag
     public void DisplayLog(String strMessage)
     {
         if (blDisplayLog)
-        {
             Debug.Log(this.entityName + ": " + strMessage);
-        }
     }
     public override void CopyPropertiesFromEntityClass()
     {
+        
         base.CopyPropertiesFromEntityClass();
         EntityClass entityClass = EntityClass.list[this.entityClass];
-        if (entityClass.Properties.Values.ContainsKey("Food"))
-            strFoodItem = entityClass.Properties.Values["Food"];
-        if (entityClass.Properties.Values.ContainsKey("Product"))
-            strProductItem = entityClass.Properties.Values["Product"];
+        if (entityClass.Properties.Values.ContainsKey("FoodItem"))
+            this.strFoodItem = entityClass.Properties.Values["FoodItem"];
+        if (entityClass.Properties.Values.ContainsKey("ProductItem"))
+            this.strProductItem = entityClass.Properties.Values["ProductItem"];
         if (entityClass.Properties.Values.ContainsKey("HarvestItems"))
-            strHarvestItems = entityClass.Properties.Values["HarvestItems"];
+            this.strHarvestItems = entityClass.Properties.Values["HarvestItems"];
+        if (entityClass.Properties.Values.ContainsKey("HomeBlock"))
+            this.strHomeBlock = entityClass.Properties.Values["HomeBlock"];
+
     }
     public override void OnUpdateLive()
     {
         base.OnUpdateLive();
-        // We only want to do this check periodically to avoid unnecessary overhad.
-        if (nextCheck < Time.time)
-        {
-            DisplayLog("OnUpdateLive() Checking Bounds");
-            nextCheck = Time.time + CheckDelay;
-            updateBlockRadiusEffects();
 
+        // We only want to do this check periodically to avoid unnecessary overhad.
+        if (nextLongerCheck < Time.time)
+        {
+            nextLongerCheck = Time.time + LongerDelay;
+            FindHomePosition();
+        }
+
+        // Quicker update to check if there's any entities around
+        if (CheckDelay < Time.time)
+        {
+            DisplayLog(" EntityID: " + this.entityId + " Health: " + base.Stats.Health + " Stamina: " + base.Stats.Stamina + " Hunger: " + " Thirsty: ");
             // If the entity can see a player, and the player is holding its food item, start moving towards the player.
-            List<Entity> entitiesInBounds = GameManager.Instance.World.GetEntitiesInBounds(this, new Bounds(this.position, Vector3.one * 20f));
+            List<Entity> entitiesInBounds = GameManager.Instance.World.GetEntitiesInBounds(this, new Bounds(this.position, Vector3.one * MaxDistanceToSeePlayer));
             if (entitiesInBounds.Count > 0)
             {
                 for (int i = 0; i < entitiesInBounds.Count; i++)
@@ -67,7 +85,7 @@ class EntityFarmingAnimal : EntityAnimalStag
                         DisplayLog("Player is holding: " + entityPlayer.inventory.holdingItem.Name);
                         if (entityPlayer.inventory.holdingItem.Name == strFoodItem)
                         {
-                            DisplayLog(" Player is holding Food!");
+                            DisplayLog(" Player is holding Food! Move towards the player");
                             SetInvestigatePosition(entityPlayer.GetBlockPosition().ToVector3(), 300);
                         }
 
@@ -79,8 +97,11 @@ class EntityFarmingAnimal : EntityAnimalStag
         }
     }
 
-    private void updateBlockRadiusEffects()
+    // If the animal has a home block set, then look around for it.
+    private void FindHomePosition()
     {
+        if (String.IsNullOrEmpty(this.strHomeBlock))
+            return;
         Vector3i blockPosition = base.GetBlockPosition();
         int num = global::World.toChunkXZ(blockPosition.x);
         int num2 = global::World.toChunkXZ(blockPosition.z);
@@ -95,11 +116,15 @@ class EntityFarmingAnimal : EntityAnimalStag
                     for (int k = 0; k < tileEntities.list.Count; k++)
                     {
                         TileEntityLootContainer tileEntity = tileEntities.list[k] as TileEntityLootContainer;
-    
                         if (tileEntity != null)
                         {
                             DisplayLog("Tile Entity is a Loot Container");
                             BlockValue block = this.world.GetBlock(tileEntity.ToWorldPos());
+
+                            // If it's not the entities home block, move to the next one.
+                            if (block.Block.GetBlockName() != this.strHomeBlock)
+                                continue;
+
                             Block block2 = Block.list[block.type];
                             if (block2.RadiusEffects != null)
                             {
@@ -107,17 +132,16 @@ class EntityFarmingAnimal : EntityAnimalStag
                                 float distanceSq = base.GetDistanceSq(tileEntity.ToWorldPos().ToVector3());
                                 for (int l = 0; l < block2.RadiusEffects.Length; l++)
                                 {
+                                    
                                     BlockRadiusEffect blockRadiusEffect = block2.RadiusEffects[l];
-                                    if (distanceSq <= blockRadiusEffect.radius * blockRadiusEffect.radius)
+                                    if (blockRadiusEffect.variable == "buffFoodTrough")
                                     {
-                                        
-                                        this.setHomeArea(tileEntity.ToWorldPos(), 15);
-                                        DisplayLog("Setting Home Position: " + this.getHomePosition() );
 
-                                        DisplayLog(" Close Enough To Feed Trough");
-                                        // If the feeding bin has food, reset the despawn time.
-                                        if (CheckFoodBox((TileEntityLootContainer)tileEntity))
-                                            ResetDespawnTime();
+                                        if (distanceSq <= blockRadiusEffect.radius * blockRadiusEffect.radius)
+                                        {
+                                            this.setHomeArea(tileEntity.ToWorldPos(), MaxDistanceFromHome);
+                                            DisplayLog("Setting Home Position: " + this.getHomePosition());
+                                        }
                                     }
                                 }
                             }
@@ -130,6 +154,35 @@ class EntityFarmingAnimal : EntityAnimalStag
 
     }
 
+    private void EventData_Event(object obj)
+    {
+        object[] array = (object[])obj;
+
+        // bring up the player's UI to show the timer.
+        EntityPlayerLocal entityPlayerLocal = array[0] as EntityPlayerLocal;
+        LocalPlayerUI uiforPlayer = LocalPlayerUI.GetUIForPlayer(entityPlayerLocal);
+
+        ItemStack itemStack = new ItemStack(ItemClass.GetItem(this.strProductItem, false), 1);
+        if (!uiforPlayer.xui.PlayerInventory.AddItem(itemStack, true))
+        {
+            uiforPlayer.xui.PlayerInventory.DropItem(itemStack);
+        }
+    }
+
+    public void HarvestWithTimer( EntityAlive _player)
+    {
+        LocalPlayerUI playerUI = (_player as EntityPlayerLocal).PlayerUI;
+        playerUI.windowManager.Open("timer", true, false, true);
+        XUiC_Timer childByType = playerUI.xui.GetChildByType<XUiC_Timer>();
+        TimerEventData timerEventData = new TimerEventData();
+        timerEventData.Data = new object[]
+        {
+            _player
+        };
+        timerEventData.Event += this.EventData_Event;
+        childByType.SetTimer(this.HarvestDelay, timerEventData);
+    }
+
     public bool CheckFoodBox(TileEntityLootContainer tileLootContainer)
     {
         if (tileLootContainer.items != null)
@@ -140,43 +193,38 @@ class EntityFarmingAnimal : EntityAnimalStag
                 if (array[i].IsEmpty())
                     continue;
 
+                // The animals will only eat the food they like best.
                 if (array[i].itemValue.ItemClass.Name == this.strFoodItem)
                 {
                     DisplayLog(" Feeding Trough has food");
-                    if (array[i].count == 1)
-                    {
-                        tileLootContainer.RemoveItem(array[i].itemValue);
-                        DisplayLog("Consumed a food.");
-                    }
-                    else
-                    {
-                        DisplayLog("Decrementing Food by 1");
-                        array[i].count--;
-                        //  tileLootContainer.UpdateSlot(i, array[i].Clone());
+                    DisplayLog("Consuming food.");
 
-                    }
+                    // if there's only one left, remove the entire item; otherwise, decrease it.
+                    if (array[i].count == 1)
+                        tileLootContainer.RemoveItem(array[i].itemValue);
+                    else
+                        array[i].count--;
                     return true;
                 }
             }
         }
 
-        return true;
+        return false;
     }
 
     public override EntityActivationCommand[] GetActivationCommands(Vector3i _tePos, EntityAlive _entityFocusing)
     {
         if (this.IsDead())
-        {
             return new EntityActivationCommand[0];
-        }
+
         return new EntityActivationCommand[]
         {
             new EntityActivationCommand("Pet", "hand", true),
-            new EntityActivationCommand("Milk", "search", true)
+            new EntityActivationCommand("Milk", "hand", true)
+
         };
     }
 
-   
     public override bool OnEntityActivated(int _indexInBlockActivationCommands, Vector3i _tePos, EntityAlive _entityFocusing)
     {
         if ( _indexInBlockActivationCommands == 0 )
@@ -187,18 +235,16 @@ class EntityFarmingAnimal : EntityAnimalStag
         {
             // Grab a reference to whatever item the player is holding
             String strHoldingItem = _entityFocusing.inventory.holdingItem.Name;
+            DisplayLog("OnEntityActivated: Holding Item: " + strHoldingItem);
 
             // If it's not in the list of valid items, don't do anything.
             if (!this.strHarvestItems.Contains(strHoldingItem))
                 return false;
 
-            DisplayLog("OnEntityActivated: Holding Item: " + strHoldingItem);
-
             if (productNextCheck < Time.time)
             {
                 productNextCheck = Time.time + ProductDelay;
-                ItemValue item = ItemClass.GetItem(this.strProductItem, false);
-                _entityFocusing.inventory.SetItem(_entityFocusing.inventory.holdingItemIdx, new ItemStack(item, _entityFocusing.inventory.holdingCount));
+                HarvestWithTimer(_entityFocusing);
             }
         }
 
