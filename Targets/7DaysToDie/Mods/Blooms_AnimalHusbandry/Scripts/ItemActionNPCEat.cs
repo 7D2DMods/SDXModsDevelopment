95b228ffc1c153a951e1304f25bc7d4be6115aca
Jeff Kilpatrick <kilpatrick.jeff@gmail.com>
2019-02-17 09:29:28 -0400

    switching back to laptop

--- Targets/7DaysToDie/Mods/Blooms_AnimalHusbandry/Scripts/ItemActionNPCEat.cs
+++ Targets/7DaysToDie/Mods/Blooms_AnimalHusbandry/Scripts/ItemActionNPCEat.cs
@@ -3,13 +3,25 @@ using UnityEngine;
 class ItemActionExchangeItemSDX: ItemActionExchangeItem
 {
 
+    private bool isFocusingBlock(WorldRayHitInfo _hitInfo)
+    {
+        for (int i = 0; i < this.focusedBlocks.Count; i++)
+        {
+            BlockValue other = this.focusedBlocks[i];
+            if (_hitInfo.hit.blockValue.Equals(other))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public override void ExecuteAction(ItemActionData _actionData, bool _bReleased)
     {
         Debug.Log("ExecuteAction ExchangeItemSDX");
-        Debug.Log(" Released?: " + _bReleased);
-        Debug.Log(" Last Use Count: " + _actionData.lastUseTime);
-        if ( _bReleased && _actionData.lastUseTime > 0f )
+        if (_bReleased && Voxel.voxelRayHitInfo.bHitValid && this.isFocusingBlock(Voxel.voxelRayHitInfo))
         {
+
             Debug.Log("Checking actions");
             if (_actionData.indexInEntityOfAction == 0)
             {
