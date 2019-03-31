25619ea7e02b1ff86ba35df69068991ec477d274
Jeff Kilpatrick <kilpatrick.jeff@gmail.com>
2019-03-31 19:08:40 -0300

    Create EAIApproachAndAttackSDX.cs

---
+++ Targets/7DaysToDie/Mods/SDX_EAITasks/Scripts/EAIApproachAndAttackSDX.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class EAIApproachAndAttackSDX : EAIApproachAndAttackTarget
+{
+    private bool isTargetToEat = false;
+    public override void Start()
+    {
+        base.Start();
+        isTargetToEat = false;
+    }
+
+}
+
