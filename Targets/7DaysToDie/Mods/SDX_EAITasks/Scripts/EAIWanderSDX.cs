001351de6a6ea60d78eaf021da44026ba16ed0f0
Jeff Kilpatrick <kilpatrick.jeff@gmail.com>
2019-04-15 07:35:02 -0300

    resync

--- Targets/7DaysToDie/Mods/SDX_EAITasks/Scripts/EAIWanderSDX.cs
+++ Targets/7DaysToDie/Mods/SDX_EAITasks/Scripts/EAIWanderSDX.cs
@@ -31,8 +31,14 @@ class EAIWanderSDX : EAIWander
 
         // if an entity gets 'stuck' on a block, it just starts attacking it. Kind of aggressive.
         if (this.theEntity.moveHelper.BlockedTime <= 1f)
+        {
+            this.theEntity.navigator.clearPath();
+            Vector3 headPosition = this.theEntity.getHeadPosition();
+            Vector3 vector = this.theEntity.GetForwardVector();
+            vector = Quaternion.Euler(UnityEngine.Random.value * 120f - 30f, UnityEngine.Random.value * 120f - 60f, 0f) * vector;
+            this.theEntity.SetLookPosition(headPosition + vector);
             return false;
-
+        }
 
         return base.Continue();
     }
